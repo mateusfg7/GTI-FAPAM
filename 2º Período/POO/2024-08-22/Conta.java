348ff7ab47b06809b05c760a8b9a5ec4ecb0f739
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-08-22 20:29:35 -0300

    add getters and setters

--- 2º Período/POO/2024-08-22/Conta.java
+++ 2º Período/POO/2024-08-22/Conta.java
@@ -1,14 +1,26 @@
 public class Conta {
-  double saldo;
-  String cliente;
-  int numConta;
-  double chequeEspecial;
+  private double saldo;
+  private String cliente;
+  private int numConta;
+  private double chequeEspecial;
 
-  String extrato() {
+  // gets e sets
+  // get geralmente não tem parâmetros e tem o tipo de retorno
+  // set geralmente tem um parâmetro e não tem retorno
+
+  public double getChequeEspecial() {
+    return chequeEspecial;
+  }
+
+  public void setChequeEspecial(double chequeEspecial) {
+    this.chequeEspecial = chequeEspecial;
+  }
+
+  public String extrato() {
     return "Cliente " + cliente + " tem saldo de R$" + saldo + " reais.";
   }
 
-  void transferir(Conta destino, double valor) {
+  public void transferir(Conta destino, double valor) {
     saldo -= valor;
     destino.saldo += valor;
   }
