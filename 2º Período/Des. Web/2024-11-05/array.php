0f6022f5013ec89a1dc5fb9d12d71dbb04bad740
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-11-12 22:07:35 -0300

    fix lamp

---
+++ 2º Período/Des. Web/2024-11-05/array.php
@@ -0,0 +1,10 @@
+<pre>
+
+  <?php
+
+  $a = ['Valor', 'Valor 2'];
+  print_r($a);
+
+
+  ?>
+</pre>
---
+++ 2º Período/Des. Web/2024-11-12/index.php
@@ -0,0 +1,2 @@
+<?php
+$conn = mysqli_connect('db', 'root', 'root_password', '2024_11_12-DevWeb') or die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
